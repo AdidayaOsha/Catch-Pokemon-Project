b145c9fd83edefb68df16bcdc568eb3ad7ed89cf
AdidayaOsha <adidayaosha@gmail.com>
2022-06-22 15:48:08 +0700

    File Initialization

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-sm font-bold">Pokemon Project</h1>
+    </div>
+  );
+}
+
+export default App;
